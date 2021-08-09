2e4ebc6170642a627c4613f314212e8394302596
Talha Rao <60220164+Talha-Rao@users.noreply.github.com>
2021-08-09 19:58:41 +0500

    Update sonarqube-scanner.js

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -2,8 +2,7 @@ const scanner = require('sonarqube-scanner');
 scanner(
   {
   serverUrl: "http://localhost:9000",
-  login:"admin",
-  password:"Sonarqube",
+  Token:"440866a9e923c050ba624340abf3bcce4aad0541",
   options: {
     "sonar.sources": "./src"
   },
